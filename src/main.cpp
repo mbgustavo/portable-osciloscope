579aa4173e3712e52602e7bc02cce68143d760a9
Gustavo <mbtt.gustavo@gmail.com>
2026-08-05 20:39:59 -0300

    Add icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
+#include <QIcon>
 
 #include "MainWindow.h"
 
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
+  QApplication::setWindowIcon(QIcon(":/icons/signal.svg"));
   MainWindow window;
   window.show();
   return QApplication::exec();
